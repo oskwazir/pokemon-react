efae794e9da82b19512bfdd1a68af42b28dd3d94
Omer Wazir <omer.wazir@gmail.com>
2015-02-05 12:23:44 -0700

    Added test for SearchBar component

---
+++ __tests__/SearchBar-test.js
@@ -0,0 +1,32 @@
+jest.dontMock('../build/SearchBar.react')
+var util = require('util');
+describe('SearchBar', function(){
+  var React, TestUtils, SearchBar, fakeInputHandler;
+  
+  beforeEach(function(){
+    React = require('react/addons');
+    TestUtils = React.addons.TestUtils;
+    SearchBar = require('../build/SearchBar.react');
+    fakeInputHandler = function(){};
+  });
+
+  it('displays a form', function(){
+    var searchBarElement = TestUtils.renderIntoDocument(<SearchBar 
+                        filterText=""
+                        onUserInput={this.fakeInputHandler} />);
+
+    var result = React.findDOMNode(searchBarElement);
+    expect(result).toNotEqual(null);
+    expect(result.nodeName).toEqual('FORM');
+    expect(result.nodeType).toEqual(1);
+
+  });
+
+  it('and the form has an input field', function(){
+    var searchBarElement = TestUtils.renderIntoDocument(<SearchBar 
+                        filterText="Search"
+                        onUserInput={this.fakeInputHandler} />);
+    var result = React.findDOMNode(searchBarElement);
+    expect(result.children[0].nodeName).toEqual('INPUT');
+  });
+})
--- src/js/SearchBar.react.js
+++ src/js/SearchBar.react.js
@@ -10,18 +10,15 @@ class SearchBar extends React.Component {
         );
     }
     render() {
-        return (<div className="wrapper">
-                    <header className="header"><h1>Pokedex</h1></header>
-                    <form>
-                        <input 
-                            value={this.props.filterText}
-                            onChange={this.handleChange}
-                            ref="filterTextInput"
-                            className="searchBar h1" 
-                            type="text" 
-                            placeholder="Search..." />
-                    </form>
-                </div>);
+        return (<form>
+                    <input 
+                        value={this.props.filterText}
+                        onChange={this.handleChange}
+                        ref="filterTextInput"
+                        className="searchBar h1" 
+                        type="text" 
+                        placeholder="Search..." />
+                </form>);
     }
 };
 module.exports = SearchBar;
