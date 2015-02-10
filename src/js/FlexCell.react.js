fb8a514fd2ec9fdf8304f185fb49499c869f34cb
Omer Wazir <omer.wazir@gmail.com>
2015-02-10 00:54:30 -0700

    Removed image

--- src/js/FlexCell.react.js
+++ src/js/FlexCell.react.js
@@ -3,7 +3,6 @@ var React = require('react');
 class FlexCell extends React.Component{
     render(): any  {
         return <div className="cell animated fadeIn">
-          <img src="/img/pokeball.svg" alt="Pokeball expands into details" />
           <p>{this.props.title}</p>
         </div>;
     }
