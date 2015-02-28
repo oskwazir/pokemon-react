539331e0649297fcc7e1cb01822018d01a5991d4
Omer Wazir <omer.wazir@gmail.com>
2015-02-28 14:33:41 -0700

    Basic dispatcher

---
+++ src/js/dispatchers/app-dispatcher.js
@@ -0,0 +1,14 @@
+const Dispatcher = require('flux').Dispatcher;
+const assign = require('object-assign');
+
+const AppDispatcher = assign(new Dispatcher, {
+    handleViewAction(action){
+    console.log('action', action);
+    this.dispatch({
+      source: 'VIEW_ACTION',
+      action:action
+    })
+  }
+});
+
+module.exports = AppDispatcher;
